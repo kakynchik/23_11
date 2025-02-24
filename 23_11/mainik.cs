01ab80093f01e183c4e40b3f4bd391ee522b2fab
kakynchik <ukakyn@gmail.com>
2025-02-24 21:20:48 +0200

    initialize project structure with .gitignore, solution, and main program files

---
+++ 23_11/mainik.cs
@@ -0,0 +1,21 @@
+﻿using _23_11;
+namespace _23_11
+{
+    class Program
+    {
+        static void Main()
+        {
+            Task1dz task1dz = new Task1dz();
+            task1dz.Execute();
+
+            Task2dz task2dz = new Task2dz();
+            task2dz.Execute();
+
+            Task3dz task3dz = new Task3dz();
+            task3dz.Execute();
+            
+            Task4dz task4dz = new Task4dz();
+            task4dz.Execute();
+        }
+    }
+}
